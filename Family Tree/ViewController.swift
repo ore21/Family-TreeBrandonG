eb5dc91752b77842f7908b5eb0ebb6e6c6ca68fc
ore21 <bgonzalez526@students.mchenry.edu>
2019-02-19 19:37:59 -0600

    Initial Commit

---
+++ Family Tree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Family Tree
+//
+//  Created by student on 2/19/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
